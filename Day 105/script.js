dbffe54228a4d355ffb080db24ddd511ff439d08
merabikogaganidze-cloud <merabikogaganidze@gmail.com>
2026-05-02 19:45:38 +0400

    commit

---
+++ Day 105/script.js
@@ -0,0 +1,17 @@
+```
+1)შექმენი სამი ცვლადი,
+ერთი var ერთი const და ერთი let keyword ის გამოყენებით,ცვლადები შექმენით camelCase ით(ორი ან მეტი სიტყვა იყოს სამივე ცვლადში,
+
+სამივე ცვლადში შეინახეთ რაიმე სტრინგი
+
+შენი დავალებაა რომ გამოიტანო სამივე ცვლადში შენახული სტრინგების სიგრძე```
+
+var m1="myBadBoy"
+
+let m2="myGoodBoy"
+
+const m3="myNormalBoy"
+
+console.log(myBadBoy.length)
+console.log(myGoodBoy.length)
+console.log(myNormalBoy.length)
